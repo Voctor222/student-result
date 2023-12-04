e5de34b32d1a3115550a4770c425111866a2c430
Victor <obinnavictoronah84@gmail.com>
2023-12-04 08:14:13 +0100

    a new file was added student result.js

---
+++ student result.js
@@ -0,0 +1,72 @@
+// grades
+var gradeA=81-100
+var gradeB=71-80
+var gradeC=61-70
+var gradeD=51-60
+var gradeE=40-50
+var gradeF=0-39
+// student score
+const result =(34,55,77,100,70,49 );{
+    let maths=34
+    let english=55
+    let chemistry=77
+    let physics=100
+    let biology=70
+    let agriculture=49
+   let result=(maths,english,chemistry,physics,biology,agriculture)
+   return result 
+}
+    if(maths===50||maths >=50){
+        console.log("E")
+    }else if(maths ===34||maths<=34){
+        console.log("F")
+    }
+    else{
+        console.warn("he did't get E")
+    }
+
+    if(english===65||english>=65){
+        console.log("C")
+    }else if(english===55||english<=55){
+        console.log("D")
+    }
+    else{
+        console.warn ("he did't get C")
+    }
+
+    if(chemistry===85||chemistry>=85){
+        console.log("A")
+    }else if(chemistry===77||chemistry<=77){
+        console.log("B")
+    }
+    else {
+        console.warn("he did't get A")
+}
+
+    if(physics===75||physics>=75){
+        console.log("B")
+    }else if(physics===100|| physics<=100){
+        console.log("A")
+    }
+    else{
+        console.warn("he did't get B")
+    }
+
+    if(biology===80|| biology >=80){
+        console.log("B")
+    }else if(biology===70|| biology<=70){
+        console.log("C")
+    }
+    else{
+        console.warn("he did't get B")
+    }
+
+    if(agriculture===55|| agriculture>=55){
+        console.log("D")
+    }else if(agriculture===49|| agriculture<=49){
+        console.log("E")
+    }
+    else{
+        console.warn("he did't get D")
+    }
+    
